1ee8f34d2ccf520224556367dc833a5ed0b6406e
Luchs <Luchs@euirc.eu>
2014-02-12 16:51:01 +0100

    Use almond.js to get a smaller JS file

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,41 @@
 var gulp = require('gulp');
-var es = require('event-stream');
+var htmlreplace = require('gulp-html-replace');
 var requirejs = require('requirejs');
 
-gulp.task('builddir', function() {
-  return es.concat(
-    gulp.src([
-      'app.js',
-      'package.json',
-    ]).pipe(gulp.dest('./build')),
-    gulp.src([
-      'public/index.html',
-    ]).pipe(gulp.dest('./build/public')),
-    gulp.src([
-      'public/bower_components/requirejs/require.js',
-    ]).pipe(gulp.dest('./build/public/bower_components/requirejs'))
-  );
+gulp.task('build-node', function() {
+  gulp.src([
+    'app.js',
+    'package.json',
+  ]).pipe(gulp.dest('./build'));
 });
 
-gulp.task('build-js', ['builddir'], function(cb) {
+gulp.task('build-html', function() {
+  return gulp.src('public/index.html')
+    .pipe(htmlreplace('js', 'bundle.js'))
+    .pipe(gulp.dest('./build/public'));
+});
+
+gulp.task('build-js', function(cb) {
   optimize({
     baseUrl: 'public/app',
     mainConfigFile: 'public/app/main.js',
 
-    name: 'main',
-    out: 'build/public/app/main.js',
+    name: '../bower_components/almond/almond',
+    wrap: true,
+    include: 'main',
+    out: 'build/public/bundle.js',
     stubModules: [ 'rv', 'text' ],
   }, cb);
 });
 
-gulp.task('build-css', ['builddir'], function(cb) {
+gulp.task('build-css', function(cb) {
   optimize({
     cssIn: 'public/style.css',
     out: 'build/public/style.css',
   }, cb);
 });
 
-gulp.task('build', ['build-js', 'build-css']);
+gulp.task('build', ['build-node', 'build-html', 'build-js', 'build-css']);
 
 function optimize(options, cb) {
   requirejs.optimize(options,
